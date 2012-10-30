e1e1e6c8aaa82bc18b1865fefb8a121d081f14ae
Alexandre Parpinelli <akparpinelli@gmail.com>
2012-10-30 14:07:05 -0200

    Sort Order options
    Sort methods now alloe to developer choose between text, values and manual.

--- jquery.customselect.js
+++ jquery.customselect.js
@@ -26,8 +26,9 @@
             'main': null,
             'button': null,
             'ready': false,
-			'isReadOnly': false,
-			'ordination': "",
+            'isReadOnly': false,
+            'ordination': "",
+            'orderbyvalue': false,
             'limit': null
         };
 
@@ -57,11 +58,11 @@
 
                 if (!$this.attr.ready) $($this.attr.container).wrap('<div class="customselect_main" />');
                 $this.attr.main = $($this.attr.container).parent();
-				
+
                 if (!$this.attr.ready) $('<div class="customselect_label"></div>').appendTo($this.attr.container);
                 $this.attr.labelContainer = $($this.attr.container).find('.customselect_label')[0];
-				
-				if (!$this.attr.ready) $('<button type="button" class="customselect_button"></button>').appendTo($this.attr.container);
+
+                if (!$this.attr.ready) $('<button type="button" class="customselect_button"></button>').appendTo($this.attr.container);
 
                 $this.attr.button = $($this.attr.container).find('.customselect_button')[0];
 
@@ -70,69 +71,72 @@
                 methods.setLabel.call($this, $this.attr.label);
 
                 if (!$this.attr.ready) $('<ul class="custonselect_list close"></ul>').appendTo($this.attr.container);
-				
-				if (($($this.attr.target).attr('data-ordination') != "") && ($($this.attr.target).attr('data-ordination') != undefined)) $this.attr.ordination = $($this.attr.target).attr('data-ordination');
-				else $this.attr.ordination = 'asc';
-				
+
+                if (($($this.attr.target).attr('data-ordination') != "") && ($($this.attr.target).attr('data-ordination') != undefined)) $this.attr.ordination = $($this.attr.target).attr('data-ordination');
+                else $this.attr.ordination = 'asc';
+
+                if (($($this.attr.target).attr('data-orderbyvalue') != "") && ($($this.attr.target).attr('data-orderbyvalue') != undefined)) $this.attr.orderbyvalue = $($this.attr.target).attr('data-orderbyvalue');
+                else $this.attr.orderbyvalue = false;
+
                 $this.attr.list = $($this.attr.container).find('.custonselect_list')[0];
                 methods.overloadItens.call($this);
 
-                if($this.attr.limit) {
+                if ($this.attr.limit) {
                     methods.showList.call($this);
                     var maxHeight = $($this.attr.list).find('li').eq(0).outerHeight(true) * parseInt($this.attr.limit);
                     methods.hideList.call($this);
-                    $($this.attr.list).css({'max-height': maxHeight});
-                }                
+                    $($this.attr.list).css({ 'max-height': maxHeight });
+                }
 
-				if (($($this.attr.target).attr('data-readonly') != "") && ($($this.attr.target).attr('data-readonly') != undefined)) $this.attr.isReadOnly = $($this.attr.target).attr('data-readonly');
-				else $this.attr.isReadOnly = false;
-				
-				if($this.attr.isReadOnly) methods.toReadOnly.call($this); 
+                if (($($this.attr.target).attr('data-readonly') != "") && ($($this.attr.target).attr('data-readonly') != undefined)) $this.attr.isReadOnly = $($this.attr.target).attr('data-readonly');
+                else $this.attr.isReadOnly = false;
 
-				if (!$this.attr.ready) methods.onFocus.call($this); 
+                if ($this.attr.isReadOnly) methods.toReadOnly.call($this);
+
+                if (!$this.attr.ready) methods.onFocus.call($this);
             },
-			
-			onFocus: function() {
-				var $this = this;
 
-				$($this.attr.main).focusin(function(){
-					if($this.attr.isReadOnly) return false;
+            onFocus: function () {
+                var $this = this;
 
-					$($this.attr.main).unbind().keydown(function(e){ 
-						e.isDefaultPrevented();
-						
-						var valueLi = null;
-						var textLi = null;
-						var i = null;
-						
-						if ((($this.attr.target).attr('data-index') != "") && (($this.attr.target).attr('data-index') != undefined)) i = parseInt($($this.attr.target).attr('data-index'));
-						
-						if(e.which == 40) {
-							if(i == null) { 
-								i = 0;
-							} else if(i >= 0 && i < $this.attr.listItens.length - 1) {
-								i++;
-							}
-						} else if(e.which == 38) {
-							if(i == null) { 
-								i = 0;
-							} else if (i > 0) { 
-								i--;
-							}
-						}
-						
-						$($this.attr.target).attr('data-index', i);
-						
-						valueLi = $this.attr.listItens[i].value;
-						textLi = $this.attr.listItens[i].text;
-						
-						methods.refreshHidden.call($this, valueLi);
-						methods.hideList.call($this);
-						methods.setLabel.call($this, textLi);
-						$this.attr.onChange();
-					});
-				});
-			},
+                $($this.attr.main).focusin(function () {
+                    if ($this.attr.isReadOnly) return false;
+
+                    $($this.attr.main).unbind().keydown(function (e) {
+                        e.isDefaultPrevented();
+
+                        var valueLi = null;
+                        var textLi = null;
+                        var i = null;
+
+                        if ((($this.attr.target).attr('data-index') != "") && (($this.attr.target).attr('data-index') != undefined)) i = parseInt($($this.attr.target).attr('data-index'));
+
+                        if (e.which == 40) {
+                            if (i == null) {
+                                i = 0;
+                            } else if (i >= 0 && i < $this.attr.listItens.length - 1) {
+                                i++;
+                            }
+                        } else if (e.which == 38) {
+                            if (i == null) {
+                                i = 0;
+                            } else if (i > 0) {
+                                i--;
+                            }
+                        }
+
+                        $($this.attr.target).attr('data-index', i);
+
+                        valueLi = $this.attr.listItens[i].value;
+                        textLi = $this.attr.listItens[i].text;
+
+                        methods.refreshHidden.call($this, valueLi);
+                        methods.hideList.call($this);
+                        methods.setLabel.call($this, textLi);
+                        $this.attr.onChange();
+                    });
+                });
+            },
 
             startButton: function () {
                 var $this = this;
@@ -143,74 +147,101 @@
 
                     return false;
                 });
-				
-				$($this.attr.labelContainer).click(function (e) {
+
+                $($this.attr.labelContainer).click(function (e) {
                     if ($($this.attr.list).hasClass('close')) methods.showList.call($this);
                     else methods.hideList.call($this);
 
                     return false;
                 });
-				
+
                 $('body').click(function () {
                     $('.custonselect_list.open').removeClass('open').addClass('close');
                     $('.customselect_container.active').removeClass('active');
                 });
             },
-			
-			stopButton: function(){
-				var $this = this;
-				
-				$($this.attr.button).unbind();
-				$($this.attr.labelContainer).unbind();
-			},
+
+            stopButton: function () {
+                var $this = this;
+
+                $($this.attr.button).unbind();
+                $($this.attr.labelContainer).unbind();
+            },
 
             setLabel: function (label) {
                 var $this = this;
 
                 $($this.attr.labelContainer).text(label);
             },
-			
-			buildListItens: function(){
-				var $this = this;
-				
-				if (($($this.attr.target).attr('data-overload') != "") && ($($this.attr.target).attr('data-overload') != undefined)) { $this.attr.overload = $($this.attr.target).attr('data-overload'); }
-                var list = $this.attr.overload.split(',');
-                
-				var objListItens = [];
-				
-				$(list).each(function (i, item) {
-                    var obj = {
-						value: item.split('|')[0],
-						text: item.split('|')[1]
-					};
-					
-					objListItens.push(obj);
-                });
-				
-				var ordened = null;
 
-				function byTextAsc(a, b) { return a.text > b.text; }
-				function byTextDesc(a, b) { return a.text < b.text; }
-				
-				if($this.attr.ordination == 'desc')
-					objListItens.sort(byTextDesc);
-				else
-					objListItens.sort(byTextAsc);
-				
-				$this.attr.listItens = objListItens;
-			},
+            buildListItens: function () {
+                var $this = this;
+
+                if (($($this.attr.target).attr('data-overload') != "") && ($($this.attr.target).attr('data-overload') != undefined)) { $this.attr.overload = $($this.attr.target).attr('data-overload'); }
+                var list = $this.attr.overload.split(',');
+
+                var objListItens = [];
+
+                $(list).each(function (i, item) {
+                    var obj = {
+                        value: item.split('|')[0],
+                        text: item.split('|')[1]
+                    };
+
+                    objListItens.push(obj);
+                });
+
+                var ordened = null;
+
+                function byTextAsc(a, b) {
+                    //return a.text > b.text; 
+                    if (a.text < b.text) return -1;
+                    if (a.text > b.text) return 1;
+                    return 0;
+                }
+
+                function byTextDesc(a, b) { //return a.text < b.text; 
+                    if (a.text > b.text) return -1;
+                    if (a.text < b.text) return 1;
+                    return 0;
+                }
+
+                function byValueAsc(a, b) {
+                    //return a.text > b.text; 
+                    if ((a.value * 1) < (b.value * 1)) return -1;
+                    if ((a.value * 1) > (b.value * 1)) return 1;
+                    return 0;
+                }
+
+                function byValueDesc(a, b) { //return a.text < b.text; 
+                    if ((a.value * 1) > (b.value * 1)) return -1;
+                    if ((a.value * 1) < (b.value * 1)) return 1;
+                    return 0;
+                }
+
+                if ($this.attr.ordination != 'manual') {
+
+                    if ($this.attr.ordination == 'desc') {
+                        objListItens.sort(($this.attr.orderbyvalue ? byValueDesc : byTextDesc));
+                    } else {
+                        objListItens.sort(($this.attr.orderbyvalue ? byValueAsc : byTextAsc));
+                    }
+                }
+
+                $this.attr.listItens = objListItens;
+            },
 
             overloadItens: function () {
                 var $this = this;
-				
-				methods.buildListItens.call($this);
-				
-				$($this.attr.list).html('');
-				
-				$($this.attr.listItens).each(function (i, item) {
+
+                methods.buildListItens.call($this);
+
+                $($this.attr.list).html('');
+
+                $($this.attr.listItens).each(function (i, item) {
                     $('<li data-value="' + item.value + '"><span>' + item.text + '</span></li>').appendTo($this.attr.list);
                 });
-				
+
                 var liGroup = $($this.attr.list).find('li');
 
                 $(liGroup).click(function () {
@@ -248,25 +279,25 @@
                     }
                 });
             },
-			
-			toReadOnly: function(){
-				var $this = this;
-				
-				$($this.attr.target).attr('readonly', 'readonly');
-				$($this.attr.main).addClass('readonly');
-				methods.stopButton.call($this);
-			},
-			
-			toReadAndWrite: function(){
-				var $this = this;
-				
-				$($this.attr.target).removeAttr('readonly');
-				$($this.attr.target).removeAttr('data-readonly');
-				$($this.attr.main).removeClass('readonly');
-				$this.attr.isReadOnly = false;
-				
-				methods.startButton.call($this);
-			},
+
+            toReadOnly: function () {
+                var $this = this;
+
+                $($this.attr.target).attr('readonly', 'readonly');
+                $($this.attr.main).addClass('readonly');
+                methods.stopButton.call($this);
+            },
+
+            toReadAndWrite: function () {
+                var $this = this;
+
+                $($this.attr.target).removeAttr('readonly');
+                $($this.attr.target).removeAttr('data-readonly');
+                $($this.attr.main).removeClass('readonly');
+                $this.attr.isReadOnly = false;
+
+                methods.startButton.call($this);
+            },
 
             addNewItem: function (obj) {
                 var $this = this;
